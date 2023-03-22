24f78849b2f75dcd0f0f2ca16dadcfd66d83fba1
Jakub Mikulas <jakub@mikul.as>
2023-03-22 12:01:14 +0100

    Update Formula/cifuzz.rb
    Co-authored-by: Markus Zoppelt <markus@zoppelt.net>

--- Formula/cifuzz.rb
+++ Formula/cifuzz.rb
@@ -4,7 +4,7 @@ class Cifuzz < Formula
   url "https://github.com/CodeIntelligenceTesting/cifuzz/archive/refs/tags/v0.24.0.tar.gz"
   sha256 "e14fbeabba5bbe4ca953c65d3576b0449ebde44c8cd10c49b8ccc3c4babf3dfd"
   license "Apache-2.0"
-  head "https://github.com/CodeIntelligenceTesting/cifuzz.git"e
+  head "https://github.com/CodeIntelligenceTesting/cifuzz.git"
   version "0.24.0"
 
   depends_on "go" => :build
