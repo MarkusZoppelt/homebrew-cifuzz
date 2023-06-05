676a7665bca025bc3fa176aa13e2482fa63f2789
Markus Zoppelt <markus.zoppelt@code-intelligence.com>
2023-06-05 15:54:29 +0200

    chore: update to 0.32.0

--- Formula/cifuzz.rb
+++ Formula/cifuzz.rb
@@ -1,15 +1,15 @@
 class Cifuzz < Formula
   desc "CLI that helps you to integrate and run fuzzing based tests"
   homepage "https://docs.code-intelligence.com/ci-fuzz/ci-fuzz-overview"
-  url "https://github.com/CodeIntelligenceTesting/cifuzz/archive/refs/tags/v0.31.0.tar.gz"
-  sha256 "927aee0e3bf4ff9f177ecac09055f7cab6713c2c8a1dc29c1923baf86c7a702b"
+  url "https://github.com/CodeIntelligenceTesting/cifuzz/archive/refs/tags/v0.32.0.tar.gz"
+  sha256 "f50328d01e69097a2228b7012d755e72fdf3ddacb756b69d74894575269c9d58"
   license "Apache-2.0"
   head "https://github.com/CodeIntelligenceTesting/cifuzz.git"
 
   depends_on "go" => :build
 
   def install
-    system "go", "run", "tools/builder/builder.go", "--version", "0.31.0"
+    system "go", "run", "tools/builder/builder.go", "--version", "0.32.0"
     system "go", "run", "-tags", "installer", "cmd/installer/installer.go", "--install-dir", prefix.to_s
   end
 
